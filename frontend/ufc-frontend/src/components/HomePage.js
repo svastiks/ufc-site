b66c6aeea5a2c3ee328e60b648c5f3b9ae7a4b37
Svastik <svastiksharma13@gmail.com>
2024-10-27 14:28:25 -0400

    added navbar and temporary background picture

---
+++ frontend/ufc-frontend/src/components/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <div>
+            <header>
+                <h1>
+                    WELCOME TO ALL IN ONE UFC
+                </h1>
+            </header>
+            <main>
+                <p>Here you can find information about all that has to do with UFC. Old events, Upcoming, Betting odds, news, and a newsletter for the latest updates!</p>
+            </main>
+        </div>
+    );
+}
+
+export default HomePage;
---
+++ frontend/ufc-frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <header className="header">
+      <a href="" className="logo">UFC HUB</a>
+
+      <nav className="navbar">
+        <a href="\" className="">Current Fighters</a>
+        <a href="\" className="">Upcoming events</a>
+        <a href="\" className="">Betting ODDs</a>
+        <a href="\" className="">Twitter News</a>
+        <a href="\" className="">News</a>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
